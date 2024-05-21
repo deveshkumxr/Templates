c0f68fe0350ea4014a0310b9f43471a31d5c4c19
Devesh Kumar <devesh171002@gmail.com>
2024-05-21 17:33:33 +0530

    merge-sort-tree updated

--- snippets/merge-sort-tree.cpp
+++ snippets/merge-sort-tree.cpp
@@ -2,50 +2,57 @@
 
 using namespace std;
 using ll = long long;
-
-const int N = 5e5 + 4;
-
-vector<ll> st[4 * N + 4];
-
-// build st -- 1-based idx
-void build(int si, int ss, int se, vector<ll> &v)
+template <class T>
+class Merge
 {
-    if (se == ss)
-        st[si].push_back(v[ss - 1]);
-    else
+public:
+    vector<vector<T>> st;
+    ll n;
+
+    Merge(ll n) : n(n)
     {
+        st.resize(4 * n + 4);
+    }
+
+    void build(vector<T> &v)
+    {
+        assert(v.size() == n);
+        buildUtil(1, 1, n, v);
+    }
+
+    ll query(int l, int r, q)
+    {
+        return queryUtil(1, 1, n, l, r, q);
+    }
+
+private:
+    void buildUtil(int si, int ss, int se, vector<T> &v)
+    {
+        if (se == ss)
+        {
+            st[si].push_back(v[ss - 1]);
+        }
+        else
+        {
+            int sm = (ss + se) >> 1;
+            buildUtil(2 * si, ss, sm, v);
+            buildUtil(2 * si + 1, sm + 1, se, v);
+            merge(st[2 * si].begin(), st[2 * si].end(), st[2 * si + 1].begin(), st[2 * si + 1].end(), back_inserter(st[si]));
+        }
+    }
+
+    // change this
+    ll queryUtil(int si, int ss, int se, int l, int r, T q)
+    {
+        if (ss > r || se < l)
+        {
+            return 0;
+        }
+        if (ss >= l && se <= r)
+        {
+            return lower_bound(st[si].begin(), st[si].end(), q) - st[si].begin();
+        }
         int sm = (ss + se) >> 1;
-        build(2 * si, ss, sm, v);
-        build(2 * si + 1, sm + 1, se, v);
-        int i = 0, j = 0;
-        while (i < st[2 * si].size() && j < st[2 * si + 1].size())
-        {
-            if (st[2 * si][i] < st[2 * si + 1][j])
-                st[si].push_back(st[2 * si][i++]);
-            else
-                st[si].push_back(st[2 * si + 1][j++]);
-        }
-        while (i < st[2 * si].size())
-        {
-            st[si].push_back(st[2 * si][i++]);
-        }
-        while (j < st[2 * si + 1].size())
-        {
-            st[si].push_back(st[2 * si + 1][j++]);
-        }
+        return queryUtil(2 * si, ss, sm, l, r, q) + queryUtil(2 * si + 1, sm + 1, se, l, r, q);
     }
-}
-
-// count of elements < cur
-ll find(int si, int ss, int se, int l, int r, ll cur)
-{
-    if (ss > r || se < l)
-        return 0;
-    if (ss >= l && se <= r)
-    {
-        int cnt = lower_bound(begin(st[si]), end(st[si]), cur) - begin(st[si]);
-        return cnt;
-    }
-    int sm = (ss + se) >> 1;
-    return find(2 * si, ss, sm, l, r, cur) + find(2 * si + 1, sm + 1, se, l, r, cur);
-}
+};
